ddb652c7ca5667c8c0db304e15f8f66c668a4bab
Zanmatox <cheungkevin@hotmail.fr>
2021-04-26 23:22:33 +0200

    Mise en places premiers produits

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -22,6 +22,10 @@ class Category
      */
     private $name;
 
+    public function __toString() {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
